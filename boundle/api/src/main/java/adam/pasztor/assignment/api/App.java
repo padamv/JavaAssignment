03a201b55a2a61dfb71df648a1ce6325ad5bf4a8
Adam Pasztor <padamv91@gmail.com>
2017-05-01 17:19:38 +0200

    Assignment project structure has been defined.

---
+++ boundle/api/src/main/java/adam/pasztor/assignment/api/App.java
@@ -0,0 +1,13 @@
+package adam.pasztor.assignment.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
